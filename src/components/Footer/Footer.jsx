844f9fe7d3abfacfcbaff7d6a118006555fb2d71
Tim Dugan <tadugan@me.com>
2021-08-08 21:05:20 -0500

    added conditional rendering so the Bottom Navigation Bar doesn't render when a user is not logged in

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
-import { AddCircle, Casino, Person } from '@material-ui/icons';
+import { AddCircle, Casino, KeyboardReturnOutlined, Person } from '@material-ui/icons';
 
 const useStyles = makeStyles({
   root: {
@@ -20,40 +20,55 @@ const useStyles = makeStyles({
 
 import './Footer.css';
 import { useHistory } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 function Footer() {
 
   const classes = useStyles();
 
   const history = useHistory();
+  const user = useSelector(store => store.user)
 
   const handleClick = (destination) => {
     history.push(destination);
   }
 
+  // Conditional rendering so the Bottom Nav Bar is only rendered when the user is logged in
+  const conditionalNavBar = () => {
+    if (user.id) { 
+        return (
+          <BottomNavigation
+          showLabels
+          className={classes.stickToBottom}
+          style={{ height: '80px'}}
+          >
+              <BottomNavigationAction 
+                label="Profile" 
+                icon={<Person className={classes.icon}/>}
+                onClick={() => {handleClick('/user')}}
+              />
+              <BottomNavigationAction 
+                label="Collection" 
+                icon={<Casino className={classes.icon}/>}
+                onClick={() => {handleClick('/collection')}}
+              />
+              <BottomNavigationAction 
+                label="Add Game" 
+                icon={<AddCircle className={classes.icon}/>}
+                onClick={() => {handleClick('/boardgame')}} 
+              />
+          </BottomNavigation>
+        );
+    }
+    else {
+        console.log('this is user', user); // test
+        return;
+    }
+  }
+
   return (
     <footer className={classes.root}>
-      <BottomNavigation
-        showLabels
-        className={classes.stickToBottom}
-        style={{ height: '80px'}}
-      >
-        <BottomNavigationAction 
-          label="Profile" 
-          icon={<Person className={classes.icon}/>}
-          onClick={() => {handleClick('/user')}}
-        />
-        <BottomNavigationAction 
-          label="Collection" 
-          icon={<Casino className={classes.icon}/>}
-          onClick={() => {handleClick('/collection')}}
-        />
-        <BottomNavigationAction 
-          label="Add Game" 
-          icon={<AddCircle className={classes.icon}/>}
-          onClick={() => {handleClick('/boardgame')}} 
-        />
-      </BottomNavigation>
+        {conditionalNavBar()}
     </footer>
   );
 }
