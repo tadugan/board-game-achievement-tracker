b7a18d3a8805cdf855b9ddc52f2ad788d67c2b67
Tim Dugan <tadugan@me.com>
2021-08-10 09:29:57 -0500

    Added MUI button to register link on LoginPage

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,24 +1,33 @@
 import React from 'react';
 import LoginForm from '../LoginForm/LoginForm';
 import { useHistory } from 'react-router-dom';
+import { Button, makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  button: {
+    textAlign: "center",
+  },
+});
 
 function LoginPage() {
   const history = useHistory();
+
+  const classes = useStyles();
 
   return (
     <div>
       <LoginForm />
 
       <center>
-        <button
-          type="button"
-          className="btn btn_asLink"
-          onClick={() => {
-            history.push('/registration');
-          }}
-        >
-          Register
-        </button>
+        <Button 
+            variant="contained" 
+            type="button" 
+            className={classes.button} 
+            onClick={() => {
+              history.push('/login');
+            }}>
+            Register
+          </Button>
       </center>
     </div>
   );
--- src/components/RegisterPage/RegisterPage.jsx
+++ src/components/RegisterPage/RegisterPage.jsx
@@ -1,25 +1,34 @@
+import { Button, makeStyles } from '@material-ui/core';
 import React from 'react';
 
 import { useHistory } from 'react-router-dom';
 import RegisterForm from '../RegisterForm/RegisterForm';
 
+const useStyles = makeStyles({
+  button: {
+    textAlign: "center",
+  },
+});
+
 function RegisterPage() {
   const history = useHistory();
+
+  const classes = useStyles();
 
   return (
     <div>
       <RegisterForm />
 
       <center>
-        <button
-          type="button"
-          className="btn btn_asLink"
+        <Button 
+          variant="contained" 
+          type="button" 
+          className={classes.button} 
           onClick={() => {
             history.push('/login');
-          }}
-        >
+          }}>
           Login
-        </button>
+        </Button>
       </center>
     </div>
   );
