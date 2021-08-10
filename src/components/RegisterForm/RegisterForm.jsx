ab43059616628b7fabed1f1637ccab40f1d61954
Tim Dugan <tadugan@me.com>
2021-08-09 22:02:15 -0500

    Added MUI styling to registration page

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,5 +1,19 @@
+import { Button, Grid, makeStyles, TextField } from '@material-ui/core';
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+
+const useStyles = makeStyles({
+  root: {
+    textAlign: "center",
+  },
+  input: {
+    margin: "8px 0 8px 0",
+    width: "250px"
+  },
+  submit: {
+    margin: "16px 0 0 0"
+  },
+});
 
 function RegisterForm() {
   const [username, setUsername] = useState('');
@@ -10,6 +24,7 @@ function RegisterForm() {
 
   const errors = useSelector((store) => store.errors);
   const dispatch = useDispatch();
+  const classes = useStyles();
 
   const registerUser = (event) => {
     event.preventDefault();
@@ -34,6 +49,75 @@ function RegisterForm() {
           {errors.registrationMessage}
         </h3>
       )}
+      <Grid 
+        container 
+        spacing={0}
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Grid item xs={12}>
+          <TextField 
+            id="username" 
+            label="Username" 
+            variant="filled"
+            value={username}
+            required
+            onChange={(event) => setUsername(event.target.value)}
+            className={classes.input} 
+          /> 
+        </Grid>
+        <Grid item xs={12}>
+          <TextField 
+            id="password"
+            type="password" 
+            label="Password" 
+            variant="filled"
+            value={password}
+            required
+            onChange={(event) => setPassword(event.target.value)}
+            className={classes.input} 
+          /> 
+        </Grid>
+        <Grid item xs={12}>
+          <TextField 
+            id="firstName" 
+            label="First Name" 
+            variant="filled"
+            value={firstName}
+            required
+            onChange={(event) => setFirstName(event.target.value)}
+            className={classes.input}  
+          /> 
+        </Grid>
+        <Grid item xs={12}>
+          <TextField 
+            id="lastName" 
+            label="Last Name" 
+            variant="filled"
+            value={lastName}
+            onChange={(event) => setLastName(event.target.value)}
+            className={classes.input}  
+          /> 
+        </Grid>
+        <Grid item xs={12}>
+          <TextField 
+            id="profileImage" 
+            label="Profile Picture URL" 
+            variant="filled"
+            value={profileImage}
+            onChange={(event) => setProfileImage(event.target.value)}
+            className={classes.input}  
+          /> 
+        </Grid>
+        <Grid item xs={12}>
+        <Button variant="contained" color="primary" type="submit" className={classes.submit}>
+          Register
+        </Button>
+        </Grid>
+      </Grid>
+{/* 
+
       <div>
         <label htmlFor="username">
           Username:
@@ -93,10 +177,10 @@ function RegisterForm() {
             onChange={(event) => setProfileImage(event.target.value)}
           />
         </label>
-      </div>
-      <div>
+      </div> */}
+      {/* <div>
         <input className="btn" type="submit" name="submit" value="Register" />
-      </div>
+      </div> */}
     </form>
   );
 }
