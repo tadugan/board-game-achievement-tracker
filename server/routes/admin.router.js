044715446170bd1767fef81ccbbbc9eb94924e38
Tim Dugan <tadugan@me.com>
2021-08-12 11:57:47 -0500

    Created input fields for AdminAddAchievement

--- server/routes/admin.router.js
+++ server/routes/admin.router.js
@@ -43,4 +43,35 @@ router.post('/boardgame', rejectUnauthenticated, (req, res) => {
   
 });
 
+/**
+ * POST
+ * Admins add a new achievement to a board game in the database
+ */
+ router.post('/achievement', rejectUnauthenticated, (req, res) => {
+  const newAchievement = req.body;
+  const user = req.user;
+
+  const queryText = `
+  INSERT INTO "achievement" ("boardgame_id", "title", "requirement", "difficulty")
+	VALUES ($1, $2, $3, $4)
+  `;
+
+  if (user.authority >= 10) {
+    pool.query(queryText, [newAchievement.id, newAchievement.title, newAchievement.requirement, newAchievement.difficulty])
+    .then(reponse => {
+        res.sendStatus(201);
+    })
+    .catch(error => {
+        console.log('Error adding new achievement to database. Error:', error);
+        res.sendStatus(500);
+    });
+  }
+  else {
+      console.log('Error, insufficient authority to access this route.')
+      res.sendStatus(500);
+  }
+
+  
+});
+
 module.exports = router;
