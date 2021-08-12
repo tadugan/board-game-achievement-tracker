137d408959cbf8a2ce1e6da0e1cedbc3a3114251
Tim Dugan <tadugan@me.com>
2021-08-11 23:50:03 -0500

    added commet to POST route

--- server/routes/admin.router.js
+++ server/routes/admin.router.js
@@ -14,6 +14,7 @@ router.get('/', (req, res) => {
 
 /**
  * POST
+ * Admins add a new boardgame to the database
  */
 router.post('/boardgame', rejectUnauthenticated, (req, res) => {
   const newGame = req.body;
